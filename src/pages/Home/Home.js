b90ce3b5c807e377ce4fa518d24d5eb0cc1f6e82
Matthias Li Huankang <matthiasli1998@hotmail.com>
2022-07-08 15:28:50 +0800

    add missing stmt

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,6 +5,7 @@ import './Home.css';
 import { getRestaurants } from '../../database/api';
 import PopularSection from '../../components/Home/Popular';
 import CuisineSection from '../../components/Home/Cuisines';
+import { Card } from '../../components/ItemCard/Card';
 
 const Home = () => {
     return (
@@ -17,11 +18,11 @@ const Home = () => {
                 </Container>
             </Container>
             <div>
-                <Card 
+                {/* <Card 
                     url={"https://media.istockphoto.com/photos/table-top-view-of-spicy-food-picture-id1316145932?b=1&k=20&m=1316145932&s=170667a&w=0&h=feyrNSTglzksHoEDSsnrG47UoY_XX4PtayUPpSMunQI="} 
                     title={"Example title"} 
                     desc={"Example description"}>
-                </Card>
+                </Card> */}
             </div>
             <div>
                 <button onClick={() => getRestaurants()}>click me</button>
