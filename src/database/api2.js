50293f1cc2890e139fca950d922d3bc29aa49b3b
thewrik <wrik@u.nus.edu>
2022-07-08 16:03:20 +0800

    Update api2.js

--- src/database/api2.js
+++ src/database/api2.js
@@ -1,46 +1,17 @@
 export async function getRestaurantsByOffice(office) {
     const response = await fetch(`https://qw8s9n3udl.execute-api.ap-southeast-1.amazonaws.com/api/getrestaurantbyoffice?office=${office}`);
-    // const myJson =  response.json(); //extract JSON from the http response
-    // do something with myJson
-    const responseJson = response.json();
-    responseJson.then(value => {
-      const body = JSON.parse(value.body);
-      console.log(body);
-      return body;
-    }).catch( error => {
-      console.log(error);
-      return null;
-    });
+    const filteredrestaurants = response.json()
+    return filteredrestaurants
   }
 
-  export async function getAllFoodByRestaurantId(restaurantId) {
+export async function getAllFoodByRestaurantId(restaurantId) {
     const response = await fetch(`https://qw8s9n3udl.execute-api.ap-southeast-1.amazonaws.com/api/getallfoodbyrestaurant?id=${id}`);
-    // const myJson =  response.json(); //extract JSON from the http response
-    // do something with myJson
-    const responseJson = response.json();
-    responseJson.then(value => {
-      const body = JSON.parse(value.body);
-      console.log(body);
-      return body;
-    }).catch( error => {
-      console.log(error);
-      return null;
-    });
+    const menu = response.json()
+    return menu
   }
 
-  export async function getCommonOrders({deliverylocation, deliveredtime}) {
+export async function getCommonOrders({deliverylocation, deliveredtime}) {
     const response = await fetch(`https://qw8s9n3udl.execute-api.ap-southeast-1.amazonaws.com/api/getcommonorders?deliverylocation=${deliverylocation}&deliveredtime=${deliveredtime}`);
-    // const myJson =  response.json(); //extract JSON from the http response
-    // do something with myJson
-    const responseJson = response.json();
-    responseJson.then(value => {
-      const body = JSON.parse(value.body);
-      console.log(body);
-      return body;
-    }).catch( error => {
-      console.log(error);
-      return null;
-    });
+    const commonorders = response.json()
+    return commonorders
   }
-
-  
