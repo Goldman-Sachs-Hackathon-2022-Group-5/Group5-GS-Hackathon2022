1b9d5c009eae3af8b0dab995fbd8534470b25855
nicwongg <nicwongg@gmail.com>
2022-07-11 16:52:46 +0800

    create dummy checkout page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'antd/dist/antd.css';
 import Home from './pages/Home/Home';
 import Cuisines from './pages/Cuisines/Cuisines';
 import Restaurant from './pages/Restaurants/Restaurants';
+import Checkout from './pages/Checkout/Checkout';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/NavBar';
 
@@ -16,6 +17,7 @@ function App() {
         <Route path='/restaurants/:restaurantid' element={<Restaurant/>} />
         <Route path='/menu' element={<Home/>} />
         <Route path='/cart' element={<Home/>} />
+        <Route path='/checkout' element={<Checkout/>}/>
       </Routes>
     </Router>
   );
